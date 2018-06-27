995bc44eeb10d4b8a17c38bcb9439190390af26f
Cohen <cohenrobinson@icloud.com>
2018-06-27 19:32:17 +1000

    adding definitions to integration library

---
+++ integration.h
@@ -0,0 +1,9 @@
+// libraries for various numerical integration methods
+// author: Cohen Robinson
+// date: 27/06/18
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef double (function)(double);
