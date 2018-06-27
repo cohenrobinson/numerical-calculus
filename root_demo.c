26651509b025681024879f3aad1321e05674a2b3
Cohen <cohenrobinson@icloud.com>
2018-06-27 22:27:59 +1000

    ..

---
+++ root_demo.c
@@ -0,0 +1,9 @@
+// demo for root_finding libraries
+// author: Cohen Robinson
+// date: 27/06/18
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "root_finding.h"
---
+++ root_finding.h
@@ -0,0 +1,9 @@
+// libraries for use in root finding
+// author: Cohen Robinson
+// date: 27/06/18
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef double (function)(double);
