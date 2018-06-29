f0b007f531444dfdcbee4b3522c187739e946dbe
Cohen <cohenrobinson@icloud.com>
2018-06-29 20:58:47 +1000

    added demos of both newton raphson and false positive

--- root_finding.h
+++ root_finding.h
@@ -18,7 +18,7 @@ double grad( double (*function)(double), double x){
 double newton_raph( double (*function)(double), double x1, double x2, double xacc){
 
   double xold = x1;   // begin at x1 and move to x2
-  double xnew = xold; function(xold)/grad(function, xold);  // calc first root
+  double xnew = xold - function(xold)/grad(function, xold);  // calc first root
   int counter = 1;
 
   while (fabs(xnew-xold) >= fabs(xacc)){
@@ -33,7 +33,7 @@ double newton_raph( double (*function)(double), double x1, double x2, double xac
   }
   else{
     if (x1 < x2){
-      return rtnewt( function, x1+0.01, x2, xacc);
+      return newton_raph( function, x1+0.01, x2, xacc);
     }
     else{
       printf("Error in newton_raph: Failed to find a root in the given range!\n");
@@ -45,23 +45,23 @@ double newton_raph( double (*function)(double), double x1, double x2, double xac
 // function that runs the false position method
 double false_pos( double(*function)(double), double xl, double xr, double yacc){
   int counter = 0;
-  while (function(x_new) < yacc){     // iterate until function converges within accuracy
+  double x_new = xr;
+  while (fabs(function(x_new)) >= yacc){     // iterate until function converges within accuracy
     double y1 = function(xl);
     double y2 = function(xr);
-    if (y1*y2 > 0){
-      printf("Error in false_pos: You have not chosen two points where a line between them crosses the x-axis.");
+    if (y1 * y2 > 0){
+      printf("Error in false_pos: You have not chosen two points where a line between them crosses the x-axis.\n");
       return 0;
     }
-    double x_new = xr - y2 * (xr - xl) / (y2 - y1);
-
-    if (y1 * x_new > 0){
+    x_new = xr - y2 * (xr - xl) / (y2 - y1);
+    if (y1 * function(x_new) > 0){
       xl = x_new;
     }
     else{
       xr = x_new;
     }
-    counter++
+    counter++;
   }
-  printf("The number of iterations in False Position method were: %d", counter);
+  printf("The number of iterations in False Position method were: %d\n", counter);
   return x_new;
 }
